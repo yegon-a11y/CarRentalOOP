d6d135938ebb6253394abeab6950483fb94f7102
yegon-a11y <joshuayegon25@gmail.com>
2026-02-06 09:00:36 +0300

    Create customer.java

---
+++ customer.java
@@ -0,0 +1,9 @@
+public class Customer {
+    public String customerId;
+    public String name;
+
+    public Customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+}
